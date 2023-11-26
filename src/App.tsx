f5d19e14b0e33d55c202ef45af0b22e44528fab7
arihantjain4 <jainarihantmr@gmail.com>
2023-11-26 15:29:37 -0800

    Revert "Implement card template for staff"
    This reverts commit ae6b78904b3931ca7493361770ad797be0944b5c.

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import TeamMemberCard from "./components/TeamMemberCard";
 
 function App() {
   return (
@@ -19,11 +18,6 @@ function App() {
         >
           Learn React
         </a>
-        <br/>
-        <TeamMemberCard color={"#F2168B"}
-                        image={"https://i.postimg.cc/W3P4m9fD/Screenshot-2023-11-26-at-3-13-06-PM.png"}
-                        firstName={"Bobby"}
-                        lastName={"Hardaway"}/>
       </header>
     </div>
   );
