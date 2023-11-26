f7db97a188deee8828e71b38805024074d517a78
arihantjain4 <jainarihantmr@gmail.com>
2023-11-26 15:39:57 -0800

    Implement card template for staff

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,31 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import TeamMemberCard from "./components/TeamMemberCard";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React hihi
-        </a>
-      </header>
-    </div>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.tsx</code> and save to reload.
+          </p>
+          <a
+              className="App-link"
+              href="https://reactjs.org"
+              target="_blank"
+              rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+          <br/>
+          <TeamMemberCard color={"#F2168B"}
+                          image={"https://i.postimg.cc/W3P4m9fD/Screenshot-2023-11-26-at-3-13-06-PM.png"}
+                          firstName={"Bobby"}
+                          lastName={"Hardaway"}/>
+        </header>
+      </div>
   );
 }
 
