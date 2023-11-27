a496d2d3a2daf76a98123aa0324086cd2ee1260a
emilym <emily.markova@gmail.com>
2023-11-26 22:34:26 -0800

    switch nav to joy

--- src/NavBar.tsx
+++ src/NavBar.tsx
@@ -41,65 +41,62 @@ export default function NavBar(props: Props) {
     >
       <MVDrawer />
       <Link href="/">
-              <Box
-                component="img"
-                sx={{ height: 35, maxWidth: 160, mx: 1, borderRadius: "25%" }}
-                alt="Logo"
+        <Box
+          component="img"
+          sx={{ height: 35, maxWidth: 160, mx: 1, borderRadius: "25%" }}
+          alt="Logo"
           src={logo}
-          style={{transform:"scale(1.5)", margin:"15px"}}
-              />
-            </Link>
+          style={{ transform: "scale(1.5)", margin: "15px" }}
+        />
+      </Link>
       <Typography level="h2" sx={{ my: 2 }}>
         MVHACKS 7.0
       </Typography>
-            <Box
-              sx={{ display: { xs: "none", sm: "block" }, marginLeft: "auto" }} 
+      <Box sx={{ display: { xs: "none", sm: "block" }, marginLeft: "auto" }}>
+        <ButtonGroup
+          size="sm"
+          style={{
+            border: "2px solid rgba(255, 255, 255, 0.5)",
+            borderRadius: "11px",
+            overflow: "hidden",
+            display: "inline-block",
+          }}
+          sx={{ width: "70%" }}
+        >
+          {navItems.map((item) => (
+            <Button
+              key={item}
+              sx={{ color: "#fff", textTransform: "none" }}
+              variant="solid"
+              style={{
+                margin: 0,
+                background:
+                  "linear-gradient(to right,rgba(169,169,169, 0.5), rgba(168,168,168, 0.7), rgba(169,169,169, 0.5))",
+                border: "none",
+                fontSize: "90%",
+                boxShadow: "0 0 100px 0 rgba(169,169,169, 0.5)",
+                width: "100px",
+              }}
             >
-              <ButtonGroup
-                size="sm"
-                style={{
-                  border: "2px solid rgba(255, 255, 255, 0.5)",
-                  borderRadius: "11px",
-                  overflow: "hidden",
-                  display: "inline-block",
-                }}
-                sx={{ width: "70%" }}
-              >
-                {navItems.map((item) => (
-                  <Button
-                    key={item}
-                    sx={{ color: "#fff", textTransform: "none" }}
-                    variant="solid"
-                    style={{
-                      margin: 0,
-                      background:
-                        "linear-gradient(to right,rgba(169,169,169, 0.5), rgba(168,168,168, 0.7), rgba(169,169,169, 0.5))",
-                      border: "none",
-                      fontSize: "90%",
-                      boxShadow: "0 0 100px 0 rgba(169,169,169, 0.5)",
-                      width: "100px",
-                    }}
-                  >
-                    {item}
-                  </Button>
-                ))}
-              </ButtonGroup>
-              <Button
-                size="sm"
-
-                sx={{
-                  ml: 1,
-                  width: "95px",
-                  textTransform: "none",
-                  borderRadius: "11px",
-                }}
-                variant="solid"
-                color="success"
-                style={{ border: "none", fontSize: "90%" }}
-              >
-                Sign Up
-              </Button>
-            </Box>
+              {item}
+            </Button>
+          ))}
+        </ButtonGroup>
+        <Button
+          size="sm"
+          sx={{
+            ml: 1,
+            width: "95px",
+            textTransform: "none",
+            borderRadius: "11px",
+          }}
+          variant="solid"
+          color="success"
+          style={{ border: "none", fontSize: "90%" }}
+        >
+          Sign Up
+        </Button>
+      </Box>
     </Box>
   );
 }
