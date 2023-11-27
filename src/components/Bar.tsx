7bfc1e4b45501245e9ad577fbe70ab6892e8d8b2
Atulya Weise <atultw@outlook.com>
2023-11-26 19:08:51 -0800

    convert cards to mui

--- src/components/Bar.tsx
+++ src/components/Bar.tsx
@@ -1,37 +1,24 @@
 import React from 'react';
 import Card from './Card';
 import Positions from "../data/positions.json";
+import {Box} from "@mui/joy";
 
 interface Props {
   position: "Directors" | "Sponsorship" | "Tech" | "Event" | "Outreach",
-  color: string
 }
 
 function Bar(props: Props) {
   return (
-    <div className="bar" style={{
-        "border": "2px solid " + props.color,
-        "borderRadius": "20px",
-        "margin": "10px",
-        "outline": "none",
-        "borderColor": props.color,
-        "boxShadow": "0 0 13px " + props.color
-    }}>
-      <h2 style={{
-        "float": "left",
-        "margin": "15px",
-        "color": "#f2168b"
-      }}>{props.position}</h2>
-      <br></br>
-      <div className="cardsContainer" style={{
-        "margin": "20px",
-        "display": "inline-block"
+      <Box sx={{
+          display: "grid",
+          gridTemplateColumns: "repeat(auto-fill, minmax(200px, 1fr))",
+          width: "100%",
+          gap: "1rem",
       }}>
-      {Positions[props.position].map((person) => {
-        return <Card key={person.name} name={person.name} image_url={person.image_url} position={props.position}></Card>
-      })}
-      </div>
-    </div>
+          {Positions[props.position].map((person) => {
+              return <Card key={person.name} name={person.name} image_url={person.image_url} position={props.position} link={person.link}></Card>
+          })}
+      </Box>
   );
 }
 
--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,21 +1,38 @@
 import React from 'react';
+import AspectRatio from "@mui/joy/AspectRatio";
+import Typography from "@mui/joy/Typography";
+import Button from "@mui/joy/Button";
+import {GitHub} from "@mui/icons-material";
+import {Card as MuiCard} from "@mui/joy";
 
 interface Props {
   name: string, 
   image_url: string,
   position: string,
+    link: string,
 }
 
 function Card(props: Props) {
   return (
-    <div style={{
-      "display": "inline-block",
-      "margin": "10px"
-    }}>
-        <img width='100' height='100' src={props.image_url} alt={"A picture of " + props.name}></img>
-        <p>{props.name}</p>
-        <p>{props.position}</p>
-    </div>
+      <MuiCard>
+          <AspectRatio minHeight="120px" maxHeight="200px">
+              <img
+                  src={props.image_url}
+                  loading="lazy"
+                  alt=""
+              />
+          </AspectRatio>
+          <div>
+              <Typography level="title-lg">{props.name}</Typography>
+              <Typography level="body-sm">{props.position}</Typography>
+          </div>
+
+          {/*<CardContent orientation="horizontal">*/}
+          <Button component="a" href={props.link} startDecorator={<GitHub />}>
+              GitHub
+          </Button>
+          {/*</CardContent>*/}
+      </MuiCard>
   );
 }
 
