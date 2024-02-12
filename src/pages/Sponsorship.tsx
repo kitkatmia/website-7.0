0e698bf7207bb13ec4b746cecfaac3088b9438ef
kitkatmia <alinka.markova@gmail.com>
2024-02-11 21:02:02 -0800

    minor order change

--- src/pages/Sponsorship.tsx
+++ src/pages/Sponsorship.tsx
@@ -13,18 +13,18 @@ const Sponsorship = () => {
         <NavBar />
       </Section>
       <Section>
-        <h1>
-          MVHacks would not be possible without the generosity of our sponsors
-        </h1>
-        <Sponsors />
-      </Section>
-      <Section>
         <h1>Sponsor us!</h1>
         <div>
           <img src={"/images/Prospectus.png"} width={1150} alt="MVHacks prospectus for sponsorships. Download the full version below" />
         </div>
         <a href="/MVHacks Sponsorship Prospectus.pdf" download="MVHacks Sponsorship Prospectus.pdf">Download the Prospectus</a>
 
+      </Section>
+      <Section>
+        <h1>
+          MVHacks would not be possible without the generosity of our sponsors.
+        </h1>
+        <Sponsors />
       </Section>
       <Footer></Footer>
     </Box>
